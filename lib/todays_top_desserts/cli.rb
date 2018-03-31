01490043c5e2dd1ed54d66c98e6189b019be3a8e
github username <github email address>
2018-03-30 23:16:34 -0600

    added list_desserts method and updated call method to CLI class

--- lib/todays_top_desserts/cli.rb
+++ lib/todays_top_desserts/cli.rb
@@ -1,10 +1,25 @@
 class TodaysTopDesserts::CLI
 
   def call
-    puts "Here are today's most made desserts:"
     list_desserts
     menu
     goodbye
+  end
+
+  def list_desserts
+    puts "Here are today's most made desserts:"
+    puts <<-DOC
+      1. Isaac's Carrot Cake
+      2. Brooke's Best Bombshell Brownies
+      3. Lemon-Buttermilk Pound Cake with Aunt Evelyn's Lemon Glaze
+      4. Banana Cake VI
+      5. Chocolate Cornstarch Pudding
+      6. Chef John's Lemon Bars
+      7. Four-Ingredient Gluten-Free Italian Coconut Cookies
+      8. Perfect Pumpkin Pie
+      9. Chocolate Cookies
+      10. Irish Cream Brownies
+    DOC
   end
 
 end
