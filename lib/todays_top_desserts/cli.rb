621e6a68adec7d2a2aafc0d72934d0c0f2a752c6
github username <github email address>
2018-04-14 17:12:37 -0600

    changed case statement to if statement in menu method in cli

--- lib/todays_top_desserts/cli.rb
+++ lib/todays_top_desserts/cli.rb
@@ -8,6 +8,7 @@ class TodaysTopDesserts::CLI
 
   def list_desserts
     puts "Here are today's most made desserts:"
+    @recipes = TodaysTopDesserts::Recipe.today
   end
 
   def menu
@@ -15,30 +16,11 @@ class TodaysTopDesserts::CLI
     while input != "exit"
       puts "Please enter either the number of the recipe you'd like to view, 'list' to see the list of recipes again, or 'exit' to close the program:"
       input = gets.strip.downcase
-      case input
-      when "1"
-        puts "More info on recipe 1..."
-      when "2"
-        puts "More info on recipe 2..."
-      when "3"
-        puts "More info on recipe 3..."
-      when "4"
-        puts "More info on recipe 4..."
-      when "5"
-        puts "More info on recipe 5..."
-      when "6"
-        puts "More info on recipe 6..."
-      when "7"
-        puts "More info on recipe 7..."
-      when "8"
-        puts "More info on recipe 8..."
-      when "9"
-        puts "More info on recipe 9..."
-      when "10"
-        puts "More info on recipe 10..."
-      when "list"
+      if input.to_i > 0
+        puts @recipes[input.to_i-1]
+      elsif input == "list"
         list_desserts
-      when "exit"
+      elsif input == "exit"
       else
         "Not sure what you meant by that."
       end
